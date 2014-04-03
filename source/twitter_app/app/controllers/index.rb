a741653a45766c8e09a74e78e0b9f8f7fddd7a5f
David Stavis & Lyndsey Williams <pair+dstavis.lyndseywilliams@devbootcamp.com>
2014-04-03 11:58:36 -0700

    create controllers

--- source/twitter_app/app/controllers/index.rb
+++ source/twitter_app/app/controllers/index.rb
@@ -1,27 +1,27 @@
 get '/' do
-  # Look in app/views/index.erb
+  #welcome page
+  #if logged in -- redirect to '/users'
+  #else login
+  # if loggedin?
+  #   erb :index
+  # else
+  #   redirect '/login'
+  # end
+  # what info displayed on view?
   erb :index
 end
 
-get '/bands' do
-  @band_names = Band.all.map(&:name)
-  erb :bands
+
+get '/users/new' do
+  @user = User.create(params)
+  erb :new_user
 end
 
-post '/bands' do
-  new_band = Band.create!(name: params[:name])
-  redirect "/bands/#{new_band.id}"
-end
-
-get '/bands/new' do
-  erb :new_band
-end
-
-get '/bands/:id' do
+get '/users/:id' do
   @band = Band.find(params[:id])
   erb :show_band
 end
 
-get '/info' do
-  Demo.new(self).info
-end
+# get '/info' do
+#   Demo.new(self).info
+# end
