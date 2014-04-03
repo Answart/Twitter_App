4046619916bcc806adf58963e6fd1ccff0f33afd
David Stavis & Lyndsey Williams <pair+dstavis.lyndseywilliams@devbootcamp.com>
2014-04-03 12:30:41 -0700

    user login created

--- source/twitter_app/app/controllers/index.rb
+++ source/twitter_app/app/controllers/index.rb
@@ -1,53 +1,26 @@
+enable :sessions
 
 get '/' do
-  #welcome page
-  #if logged in -- redirect to '/users'
-  #else login
-  # if loggedin?
-  #   erb :index
-  # else
-  #   redirect '/login'
-  # end
-  # what info displayed on view?
-  erb :index
+  if loggedin?
+    erb :user_page #logged in home page
+  else
+    erb :index #login/createaccount page
+end
+
+get '/user/:id' do
+  @user = User.find(params[:id])
+  erb :user_profile
+end
+
+post 'user/login' do
+  @user = User.find_by_email(email: params[:email])  #add unique constrain to migration
+  if @user
+     if @user.password == params[:password]
+      session[:id] = @user.id
+      erb :user_profile
+    end
+  end
+  redirect to '/'
 end
 
 
-get '/users/new' do
-  @user = User.create(params)
-  erb :new_user
-end
-
-get '/users/:id' do
-  @band = Band.find(params[:id])
-  erb :show_band
-end
-
-# get '/' do
-#   # Look in app/views/index.erb
-#   erb :index
-# end
-
-# get '/bands' do
-#   @band_names = Band.all.map(&:name)
-#   erb :bands
-# end
-
-# post '/bands' do
-#   new_band = Band.create!(name: params[:name])
-#   redirect "/bands/#{new_band.id}"
-# end
-
-# get '/bands/new' do
-#   erb :new_band
-# end
-
-# get '/bands/:id' do
-#   @band = Band.find(params[:id])
-#   erb :show_band
-# end
-
-
-# get '/info' do
-#   Demo.new(self).info
-# end
