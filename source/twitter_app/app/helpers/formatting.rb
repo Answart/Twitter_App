14f178395aa86b018e8eb6355d7a35e6957753bf
Alexandra & Tyler Stauss <pair+Answart.tylerstauss@devbootcamp.com>
2014-04-03 18:45:34 -0700

    fixed the pesky bug

--- source/twitter_app/app/helpers/formatting.rb
+++ source/twitter_app/app/helpers/formatting.rb
@@ -4,6 +4,6 @@ helpers do
   end
 
   def current_user
-    @user ||= User.find(session[:user_id]) if session[:user_id]
+    @logged_in_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
